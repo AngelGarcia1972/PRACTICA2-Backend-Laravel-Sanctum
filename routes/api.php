6217be9d87606e63b8e6bf7d2bb4f73fbb91d28c
AngelGarcia1972 <angeldaga.17@gmail.com>
2026-05-27 15:38:40 -0600

    ✨ PRACTICA 3 RUTAS SPA Y PRODUCTOCONTROLLER

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductoController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
+Route::get('/productos',      [ProductoController::class, 'index']);
+Route::get('/productos/{id}', [ProductoController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
+    Route::post('/productos',         [ProductoController::class, 'store']);
+    Route::put('/productos/{id}',     [ProductoController::class, 'update']);
+    Route::delete('/productos/{id}',  [ProductoController::class, 'destroy']);
 });
